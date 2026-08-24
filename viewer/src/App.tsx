bf7ff58b10ce25f7c8157df2a782d99ba8a64b0c
Hugo <hugoolabi@gmail.com>
2026-08-23 23:25:38 -0300

    feat(discord): add /discord install route and Add Discord App topbar link

--- viewer/src/App.tsx
+++ viewer/src/App.tsx
@@ -1,7 +1,8 @@
 import { Route } from '@solidjs/router';
-import Home from './pages/Home.jsx';
-import PrivacyPolicy from './pages/PrivacyPolicy.jsx';
-import TermsOfService from './pages/TermsOfService.jsx';
+import Home from './pages/Home';
+import PrivacyPolicy from './pages/PrivacyPolicy';
+import TermsOfService from './pages/TermsOfService';
+import DiscordInstall from './pages/DiscordInstall';
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
       <Route path="/room/:token" component={Home} />
       <Route path="/privacy-policy" component={PrivacyPolicy} />
       <Route path="/terms-of-service" component={TermsOfService} />
+      <Route path="/discord" component={DiscordInstall} />
     </>
   );
 }
